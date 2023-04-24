dfd43ec8721cfaed8f4196b2c9cbe9d8a4e52744
DotCrying <Korestyshka@mail.ru>
2023-04-24 15:08:34 +0300

    header scroll refactor

--- js/landing.js
+++ js/landing.js
@@ -1,15 +1,65 @@
-let burgerBtn = document.querySelector('.burger');
-let menuList = document.querySelector('.menu');
+const burgerMenu = () => {
+    const burgerBtn = document.querySelector('.burger');
+    const menuList = document.querySelector('.menu');
+    const menuLink = document.querySelector('.menu__link');
 
-burgerBtn.addEventListener('click', () => {
-    menuList.classList.toggle('menu-opened');
-    burgerBtn.classList.toggle('active');
-})
+    const disableScroll = () => {
+        document.body.style.overflow = 'hidden';
+        document.body.style.userSelect = 'none';
+    }
+    const enableScroll = () => {
+        document.body.style.overflow = 'auto';
+        document.body.style.userSelect = 'auto';
+    }    
 
-window.addEventListener('click', event => {
-     if(!event.target.matches('.active span')) {
-     menuList.classList.remove('menu-opened');
-    burgerBtn.classList.remove('active');
-     }
-})
+    burgerBtn.addEventListener('click', () => {
+        if(burgerBtn.classList.contains('active')) {
+            menuList.classList.remove('menu-opened');
+            burgerBtn.classList.remove('active');
+            enableScroll();
+        } else {
+            menuList.classList.add('menu-opened');
+            burgerBtn.classList.add('active');
+            disableScroll();
+        }
+    })
 
+    // menuLink.addEventListener('click', () => {
+    //     document.body.style.overflow = 'auto';
+    //     document.body.style.userSelect = 'auto';
+    // })
+
+    // menuLink.addEventListener('click', enableScroll)
+
+    window.addEventListener('click', event => {
+        if(!event.target.matches('.active span')) {
+            menuList.classList.remove('menu-opened');
+            burgerBtn.classList.remove('active');
+        }
+    })
+}
+
+burgerMenu();
+
+window.onload = () => {
+
+    const onScrollHeader = () => {
+      const header = document.querySelector('.page__header_grey')
+      let prevScroll = window.pageYOffset
+      let currentScroll
+  
+      window.addEventListener('scroll', () => {
+        currentScroll = window.pageYOffset
+        const headerHidden = () => header.classList.contains('header_hidden')
+        if (currentScroll > prevScroll && !headerHidden()) {
+          header.classList.add('header_hidden')
+        }
+        if (currentScroll < prevScroll && headerHidden()) {
+          header.classList.remove('header_hidden')
+        }
+        prevScroll = currentScroll
+      })
+    }
+
+    onScrollHeader()
+};
